2a5feb4d561bef664493625a95b6c88f53dd405c
danielpdaniel <artofjquinn@gmail.com>
2022-08-17 14:17:18 -0400

    commit tree 056a9cd76ae64da3b6e28a13ea86ebe98f667a27

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+function findMatching(array, name) {
+
+const newName = name.toUpperCase()
+
+return array.filter(function(obj) {
+    return obj.toUpperCase() === newName
+})
+}
+
+function fuzzyMatch(array, letters) {
+
+const newLetters = letters.toUpperCase()
+
+return array.filter(function(obj){
+    return obj.slice(0, 2).toUpperCase() === newLetters
+})
+}
+
+function matchName(array, stringName) {
+return array.filter(function(obj){
+    return obj.name === stringName
+})
+}
